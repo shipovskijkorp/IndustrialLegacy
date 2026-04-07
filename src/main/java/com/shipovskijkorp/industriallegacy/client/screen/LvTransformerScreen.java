d53aca762744259acb65abaf0f00e8e133258a09
shipovskijkorp <shipovskijkorparation@gmail.com>
2026-04-07 23:38:59 +0300

    alpha 0.1.6

--- src/main/java/com/shipovskijkorp/industriallegacy/client/screen/LvTransformerScreen.java
+++ src/main/java/com/shipovskijkorp/industriallegacy/client/screen/LvTransformerScreen.java
@@ -51,11 +51,14 @@ public class LvTransformerScreen extends HandledScreen<LvTransformerScreenHandle
 
     @Override
     protected void drawForeground(DrawContext context, int mouseX, int mouseY) {
+        Text title = Text.literal("LV-Transformer");
+        int titleX = (this.backgroundWidth - this.textRenderer.getWidth(title)) / 2;
+        context.drawText(this.textRenderer, title, titleX, 6, 4210752, false);
         context.drawText(this.textRenderer, Text.translatable("gui.industrial_legacy.transformer.output"), 6, 30, 4210752, false);
         context.drawText(this.textRenderer, Text.translatable("gui.industrial_legacy.transformer.input"), 6, 43, 4210752, false);
 
-        context.drawText(this.textRenderer, Text.literal(handler.getOutputFlow() + " EU/t"), 52, 30, 2157374, false);
-        context.drawText(this.textRenderer, Text.literal(handler.getInputFlow() + " EU/t"), 52, 45, 2157374, false);
+        context.drawText(this.textRenderer, Text.literal(handler.getInputFlow() + " EU/t"), 52, 30, 2157374, false);
+        context.drawText(this.textRenderer, Text.literal(handler.getOutputFlow() + " EU/t"), 52, 45, 2157374, false);
 
         int wrenchY = switch (handler.getModeOrdinal()) {
             case 1 -> 87;
